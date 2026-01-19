f8ef76c1c207a4134a621fbc468da1f5c91e0e9d
ytsuchiya-work <ytsuchiya0718.work@gmail.com>
2026-01-19 01:52:20 +0000

    update

--- 0_config.py
+++ 0_config.py
@@ -10,7 +10,7 @@ index_name = f"{catalog}.{schema}.accident_report_silver_index"
 pdf_folder_name = "source"
 pdf_dir = f"/Volumes/{catalog}/{schema}/{pdf_folder_name}"
 
-curate_llm_endpoint_name = "databricks-gpt-5"
+curate_llm_endpoint_name = "databricks-gpt-oss-120b"
 embedding_endpoint = "databricks-gte-large-en"
 
 VECTOR_SEARCH_ENDPOINT_NAME = "vs-endpoint"
